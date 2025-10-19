1f2e184503a530291fb73ea875686b64c34e3908
rishabh21g <biasedengineer@gmail.com>
2025-10-19 12:22:18 +0530

    gin setup done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"sync"
-
-	"github.com/rishabh21g/mailchimp/internal/services"
-	"github.com/rishabh21g/mailchimp/internal/types"
+	"github.com/gin-gonic/gin"
+	"github.com/rishabh21g/mailchimp/internal/handlers"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	recipientChannel := make(chan types.Recipient) // unbuffered channel
-	go services.LoadRecipients("../../internal/data/email.csv", recipientChannel)
+	// var wg sync.WaitGroup
+	// recipientChannel := make(chan types.Recipient) // unbuffered channel
+	// go services.LoadRecipients("../../internal/data/email.csv", recipientChannel)
 
-	workers := 5
-	for i := 0; i <= workers; i++ {
-		wg.Add(1)
-		go services.EmailWorker(i, recipientChannel, &wg)
-
-	}
-	wg.Wait()
-	// time.Sleep(time.Second)       // temporary solution for completing the task of goroutines
-
+	// workers := 5
+	// for i := 0; i <= workers; i++ {
+	// 	wg.Add(1)
+	// 	go services.EmailWorker(i, recipientChannel, &wg)
+	// }
+	// wg.Wait()
+	// Gin server code started
+	router := gin.Default()
+	routerAuth := router.Group("/api/v1/auth" )
+	routerAuth.GET("/login",  handlers.LoginHandler )
+	routerAuth.GET("/signup",  handlers.RegisterHandler )
+	router.Run(":3000")
 }
